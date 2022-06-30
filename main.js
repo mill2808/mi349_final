5c84b4732ef0508b9bd7c630a17db09c885073ad
mill2808 <105799175+mill2808@users.noreply.github.com>
2022-06-30 00:05:17 -0400

    switch from thesis website to mi349_final

---
+++ main.js
@@ -0,0 +1,38 @@
+
+
+
+/* ********************** FAQ dropdown answers ******************************** */
+var faq = document.getElementsByClassName("faq-page");
+var i;
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+        /* Toggle between hiding and showing the active panel */
+        var body = this.nextElementSibling;
+        if (body.style.display === "block") {
+            body.style.display = "none";
+        } else {
+            body.style.display = "block";
+        }
+    });
+}
+
+/* ********************** Resource dropdown answers ******************************** */
+var resource = document.getElementsByClassName("resource-page");
+var i;
+for (i = 0; i < resource.length; i++) {
+    resource[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+        /* Toggle between hiding and showing the active panel */
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
