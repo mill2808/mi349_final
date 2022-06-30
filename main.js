dbdae543e4675553a4a65c8bcee60541a15a1a7c
mill2808 <105799175+mill2808@users.noreply.github.com>
2022-06-30 17:53:29 -0400

    navbar and touchups

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+/* ********************** Navigation menu toggle ******************************** */
+var toggleButton = document.getElementsByClassName("toggle-button");
+var i;
+for (i = 0; i < toggleButton.length; i++) {
+    toggleButton[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+        /* Toggle between hiding and showing the active panel */
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
 
 
 
